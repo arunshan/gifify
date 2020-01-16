2a3b987d47b3fe4a25e4df82d0398a7f48f021ec
arunshan <arun.kris.shankar@gmail.com>
2020-01-16 13:53:20 -0800

    Gifify video files

---
+++ index.js
@@ -0,0 +1,47 @@
+const { spawn } = require('child_process');
+const program = require('commander');
+ 
+program
+  .option('-i, --input <type>', 'Input movie file')
+  .option('-o, --output <type>', 'Output gif file', 'output.gif')
+  .option('-f, --ffmpeg <type>', 'FFMPEG path on your local filesystem', 'ffmpeg')
+  .option('-s, --scale <type>', 'Scale', '720')
+  .option('-fps, --fps <type>', 'FPS', '10')
+  .option('-start, --startTime <type>', 'Start Time', '0')
+  .option('-d, --duration <type>', 'Duration')
+  .option('-l, --loop <type>', 'Loop', '0');
+
+program.parse(process.argv);
+
+const {
+  input,
+  output,
+  ffmpeg,
+  scale,
+  fps,
+  startTime,
+  duration,
+  loop
+} = program;
+
+function validate() {
+  if (!input) throw new Error('Please enter a valid input movie file path')
+}
+validate()
+
+const cmd = `${ffmpeg} \
+-ss ${startTime} \
+${duration ? `-t ${duration}` : ' '} \
+-i ${input} \
+-vf "fps=${fps},scale=${scale}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+-loop ${loop} \
+${output}`
+
+spawn(cmd, { stdio: 'inherit' , shell: true}, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
